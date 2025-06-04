3571797e856ec68765b3d17c3643200745bbfd05
Laura <lauxavi1357@gmail.com>
2025-06-04 10:43:55 -0300

    Criando estrutura

---
+++ Atividade1/Models/Login.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Atividade1.Models
+{
+    public class Login
+    {
+        [Display(Name = "Usuário")]
+        [RegularExpression(@"\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*", ErrorMessage = "Usuário Invalido")]
+        public string? Usuario { get; set; }
+
+        [Display(Name = "Senha")]
+
+        public string? Password { get; set; }
+
+
+    }
+}
